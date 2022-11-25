b240c752f6a91d065eb44a514a4bdacaebba693b
JASH-PATEL-6566 <jashpatel6566@gmail.com>
2022-11-25 14:38:21 +0530

    2# Login and Signup page

--- frontend/pages/login/index.js
+++ frontend/pages/login/index.js
@@ -1,16 +1,44 @@
 import classes from './login.module.css';
-// import Image from 'next/image';
+import TextField from '@mui/material/TextField';
+import { Button, Divider, Paper } from '@mui/material';
+import Link from 'next/link';
+import Image from 'next/image';
+import G_img from '../../Image/G.png';
 
 export default function Login() {
     return (
         <>
-            <div className={classes.login_conatainer}>
-                <div className="image">
-                    <h1>Login</h1>
-                    {/* <Image
-                        src={ }
-                    /> */}
-                </div>
+            <div className={classes.login_container}>
+                <Paper className={classes.mid} elevation={3}>
+                    <h1>Kariero</h1>
+                    <form>
+                        <TextField
+                            required
+                            className={classes.input}
+                            label="Your Email Address"
+                            type="email"
+                        />
+                        <TextField
+                            required
+                            className={classes.input}
+                            label="Your Password"
+                            type="password">
+                        </TextField>
+                        <Button className={classes.btn} variant="contained">Login</Button>
+                    </form>
+                    <Link href="/forgot-password" className={classes.link}>Forgot Password?</Link>
+                    <p>Don't have an account? <Link className={classes.link} href="/signup">Sign Up</Link></p>
+                    <Divider variant='middle' />
+                    <Paper className={classes.other_opption} variant="outlined" >
+                        <Image
+                            src={G_img}
+                            height={30}
+                            width={30}
+                            priority='performance'
+                        />
+                        <span>Continue With Google</span>
+                    </Paper>
+                </Paper>
             </div>
         </>
     )
--- frontend/pages/signup/index.js
+++ frontend/pages/signup/index.js
@@ -1,7 +1,38 @@
+import classes from './signup.module.css';
+import TextField from '@mui/material/TextField';
+import { Button, Divider, Paper } from '@mui/material';
+import Link from 'next/link';
+
 export default function SignUp() {
     return (
         <>
-            <h1>SignUp</h1>
+            <div className={classes.login_container}>
+                <Paper className={classes.mid} elevation={3} >
+                    <h1>Kariero</h1>
+                    <form>
+                        <TextField
+                            required
+                            className={classes.input}
+                            label="Enter UserName"
+                        />
+                        <TextField
+                            required
+                            className={classes.input}
+                            label="Your Email Address"
+                            type="email"
+                        />
+                        <TextField
+                            required
+                            className={classes.input}
+                            label="Your Password"
+                            type="password">
+                        </TextField>
+                        <Button className={classes.btn} variant="contained">signup</Button>
+                    </form>
+                    <p>Already have an account?<Link className={classes.link} href="/login">Log in</Link></p>
+                    <Divider variant='middle' />
+                </Paper>
+            </div>
         </>
     )
 }
